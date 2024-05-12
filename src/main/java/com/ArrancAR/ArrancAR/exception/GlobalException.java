29afdc4e68ad843819fd7915516f28f18a889208
smateodev <sotomateo@protonmail.com>
2024-05-12 14:25:18 -0300

    arreglo de dependencias

--- src/main/java/com/ArrancAR/ArrancAR/exception/GlobalException.java
+++ src/main/java/com/ArrancAR/ArrancAR/exception/GlobalException.java
@@ -1,6 +1,5 @@
 package com.ArrancAR.ArrancAR.exception;
 
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
