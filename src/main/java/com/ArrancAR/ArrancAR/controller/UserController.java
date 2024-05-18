15e1513a2630e5b7397db1f042e3a98d187c9f85
AgostinaDucret <agos.ducret18@gmail.com>
2024-05-18 20:20:20 -0300

    Documentacion

--- src/main/java/com/ArrancAR/ArrancAR/controller/UserController.java
+++ src/main/java/com/ArrancAR/ArrancAR/controller/UserController.java
@@ -5,18 +5,36 @@ import com.ArrancAR.ArrancAR.entity.User;
 import com.ArrancAR.ArrancAR.exception.DataIntegrityViolationException;
 import com.ArrancAR.ArrancAR.exception.ResourceNotFoundException;
 import com.ArrancAR.ArrancAR.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Tag(name= "User")
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User obtained correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid id",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) throws ResourceNotFoundException {
         Optional<User> foundUser = userService.findUserById(id);
@@ -26,6 +44,17 @@ public class UserController {
             throw new ResourceNotFoundException("This user doesn't exist");
         }
     }
+
+    @Operation(summary = "Registration of a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User stored correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @PostMapping("register")
     public ResponseEntity<User> addVehicle(@RequestBody User user) throws DataIntegrityViolationException {
         Optional<User> foundUser = userService.findUserByEmail(user.getEmail());
@@ -35,6 +64,19 @@ public class UserController {
             return ResponseEntity.ok(userService.addUser(user));
         }
     }
+
+    @Operation(summary = "Updating a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @PutMapping("update")
     public ResponseEntity<String> updateUser(@RequestBody User user) throws ResourceNotFoundException {
         Optional<User> foundUser= userService.findUserById(user.getIdUser());
--- src/main/java/com/ArrancAR/ArrancAR/controller/VehicleController.java
+++ src/main/java/com/ArrancAR/ArrancAR/controller/VehicleController.java
@@ -1,10 +1,15 @@
 package com.ArrancAR.ArrancAR.controller;
 
-
 import com.ArrancAR.ArrancAR.entity.Vehicle;
 import com.ArrancAR.ArrancAR.exception.DataIntegrityViolationException;
 import com.ArrancAR.ArrancAR.exception.ResourceNotFoundException;
 import com.ArrancAR.ArrancAR.service.VehicleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name= "Vehicle")
 @RestController
 @CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/vehicle")
@@ -20,6 +26,18 @@ public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
+    @Operation(summary = "List of id vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehicle obtained correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Vehicle.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid id",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Vehicle not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Vehicle>> getVehicleById(@PathVariable Long id) throws ResourceNotFoundException {
         Optional<Vehicle> foundVehicle = vehicleService.findVehicleById(id);
@@ -30,11 +48,31 @@ public class VehicleController {
         }
     }
 
+   @Operation(summary = "List of all vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehicle list obtained correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Vehicle.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("/all")
     public List<Vehicle> listVehicles() {
         return vehicleService.listVehicles();
     }
 
+   @Operation(summary = "Registration of a new car")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Vehicle stored correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Vehicle.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Vehicle> addVehicle(@RequestBody Vehicle vehicle) throws DataIntegrityViolationException {
         System.out.println(vehicle);
@@ -46,6 +84,18 @@ public class VehicleController {
         }
     }
 
+   @Operation(summary = "Deleting a vehicle by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Vehicle removed correctly",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid id",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Vehicle not foundo",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteVehicle(@PathVariable Long id) throws ResourceNotFoundException {
         Optional<Vehicle> foundVehicle = vehicleService.findVehicleById(id);
