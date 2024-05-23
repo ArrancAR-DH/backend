7fc360ebc66d0273abc25aac8aa845ce620d73e1
Tomas Lovato <lovato.tomas94@gmail.com>
2024-05-22 21:39:30 -0300

    cross origin

--- src/main/java/com/ArrancAR/ArrancAR/controller/AuthController.java
+++ src/main/java/com/ArrancAR/ArrancAR/controller/AuthController.java
@@ -6,14 +6,12 @@ import com.ArrancAR.ArrancAR.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
     private AuthService authService;
 
@@ -24,6 +22,7 @@ public class AuthController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
     @PostMapping("/login")
+
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         String response = authService.login(loginDto);
         return new ResponseEntity<>(response, HttpStatus.OK);
