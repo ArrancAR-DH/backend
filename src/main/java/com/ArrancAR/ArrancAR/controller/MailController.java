c885ad33b0b9601ede833739ad05f1e4b2115e1d
smateodev <sotomateo@protonmail.com>
2024-06-08 09:14:38 -0300

    validación de email de usuario antes de mandar el mail de bienvenida

--- src/main/java/com/ArrancAR/ArrancAR/controller/MailController.java
+++ src/main/java/com/ArrancAR/ArrancAR/controller/MailController.java
@@ -1,10 +1,15 @@
 package com.ArrancAR.ArrancAR.controller;
 
 import com.ArrancAR.ArrancAR.dto.EmailDto;
+import com.ArrancAR.ArrancAR.entity.User;
 import com.ArrancAR.ArrancAR.service.EmailService;
+import com.ArrancAR.ArrancAR.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -15,10 +20,22 @@ public class MailController {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/send")
     public ResponseEntity<String> receiveRequestEmail(@RequestBody EmailDto emailDto) {
+        Optional<User> foundUser = userService.findUserByEmail(emailDto.getToUser());
 
-        emailService.sendEmail(emailDto.getToUser(), emailDto.getFullName());
-        return ResponseEntity.ok("Email enviado correctamente");
+        if (foundUser.isPresent()) {
+            return ResponseEntity.badRequest().body("User already exists with email: " + emailDto.getToUser());
+        }
+
+        try {
+            emailService.sendEmail(emailDto.getToUser(), emailDto.getFullName());
+            return ResponseEntity.ok("Email sent successfully to " + emailDto.getToUser());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending email: " + e.getMessage());
+        }
     }
 }
